b96b7ef609b7e58b22191b61176f1feadfa03ae6
SOPESA IT <edgar.flores@sopesa.com.gt>
2014-07-24 19:26:37 -0600

    Update 24.07.2014 19:25

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,18 +1,42 @@
 from django.contrib import admin
 from tasks.models import Dominio, DominioValor, Empresa, Agencia, Sistema, Subsistema, Modulo, Recurso, RecursoSistema, ColorIssue, Issue, IssueReferencia, IssueActividad, IssueEmpresa
+from tasks import forms
 
 class DominioValorInLine(admin.TabularInline):
     model = DominioValor
 
 class DominioAdmin(admin.ModelAdmin):
     list_display = ('dominio_id', 'descripcion', 'activo',)
-    search_fields = ['dominio_id',]
     list_filter = ('dominio_id',)
+    search_fields = ['dominio_id',]
+    sortable_field_name = "dominio_id"
     inlines = [DominioValorInLine,]
 
+class AgenciaInLine(admin.TabularInline):
+    model = Agencia
+    form = forms.AgenciaForm
+    extra = 3
+
+class EmpresaAdmin(admin.ModelAdmin):
+    list_display = ('corporacion', 'empresa_id', 'pais', 'activo',)
+    list_filter = ('empresa_id',)
+    search_fields = ['empresa_id',]
+    sortable_field_name = "empresa_id"
+    inlines = [AgenciaInLine,]
+
+class AgenciaAdmin(admin.ModelAdmin):
+    fields = ('empresa', 'descripcion', 'activo')
+    list_display = ('empresa', 'descripcion', 'activo',)
+    list_filter = ('descripcion',)
+    search_fields = ['descripcion',]
+    sortable_field_name = ('empresa', 'descripcion',)
+
+class AgenciaAdmin(admin.ModelAdmin):
+    exclude = ('agencia_id',)
+
 admin.site.register(Dominio, DominioAdmin)
-admin.site.register(Empresa)
-admin.site.register(Agencia)
+admin.site.register(Empresa, EmpresaAdmin)
+admin.site.register(Agencia, AgenciaAdmin)
 admin.site.register(Sistema)
 admin.site.register(Subsistema)
 admin.site.register(Modulo)
--- tasks/models.py
+++ tasks/models.py
@@ -26,34 +26,58 @@ class DominioValor(models.Model):
         verbose_name_plural = u'Dominio Valores'
         ordering = ('dominio', 'valor',)
     def __unicode__(self):
-        return self.valor
+        return u"%s - %s" % (self.dominio, self.valor)
+
+class Corporacion(models.Model):
+    corporacion_id = models.CharField(primary_key=True, max_length=40)
+    class Meta:
+        managed = False
+        db_table = 'vtask_corporaciones'
+        verbose_name = u'Corporacion'
+        verbose_name_plural = u'Corporaciones'
+        ordering = ('corporacion_id',)
+    def __unicode__(self):
+        return self.corporacion_id
+
+class Pais(models.Model):
+    pais_id = models.CharField(primary_key=True, max_length=40)
+    class Meta:
+        managed = False
+        db_table = 'vtask_paises'
+        verbose_name = u'Pais'
+        verbose_name_plural = u'Paises'
+        ordering = ('pais_id',)
+    def __unicode__(self):
+        return self.pais_id
 
 class Empresa(models.Model):
     empresa_id = models.CharField(primary_key=True, max_length=40)
-    corporacion = models.CharField(max_length=40)
-    pais = models.CharField(max_length=40)
+    corporacion = models.ForeignKey(Corporacion)
+    pais = models.ForeignKey(Pais)
     activo = models.BooleanField(default=True)
     class Meta:
         managed = False
         db_table = 'task_empresas'
         verbose_name = u'Empresa'
         verbose_name_plural = u'Empresas'
-        ordering = ('empresa_id',)
+        ordering = ('corporacion', 'empresa_id',)
     def __unicode__(self):
-        return self.empresa_id
+        return u"%s - %s" % (self.corporacion, self.empresa_id)
 
 class Agencia(models.Model):
     empresa = models.ForeignKey(Empresa)
-    agencia_id = models.CharField(max_length=40)
+    agencia_id = models.IntegerField(primary_key=True, blank=True, null=True)
+    descripcion = models.CharField(max_length=40)
     activo = models.BooleanField(default=True)
     class Meta:
         managed = False
         db_table = 'task_agencias'
+        unique_together = (('empresa', 'agencia_id'),)
         verbose_name = u'Agencia'
         verbose_name_plural = u'Agencias'
         ordering = ('empresa', 'agencia_id',)
     def __unicode__(self):
-        return self.agencia_id
+        return u"%s - %s" % (self.empresa, self.descripcion)
 
 class Sistema(models.Model):
     sistema_id = models.CharField(primary_key=True, max_length=40)
@@ -69,30 +93,32 @@ class Sistema(models.Model):
 
 class Subsistema(models.Model):
     sistema = models.ForeignKey(Sistema)
-    subsistema_id = models.CharField(max_length=40)
+    subsistema_id = models.CharField(primary_key=True, max_length=40)
     activo = models.BooleanField(default=True)
     class Meta:
         managed = False
         db_table = 'task_subsistemas'
+        unique_together = ('sistema', 'subsistema_id')
         verbose_name = u'SubSistema'
         verbose_name_plural = u'SubSistemas'
         ordering = ('sistema', 'subsistema_id',)
     def __unicode__(self):
-        return self.subsistema_id
+        return u"%s - %s" % (self.sistema, self.subsistema_id)
 
 class Modulo(models.Model):
     sistema = models.ForeignKey(Subsistema, related_name='Modulo_sistemas')
     subsistema = models.ForeignKey(Subsistema, related_name='Modulo_subsistemas')
-    modulo_id = models.CharField(max_length=40)
+    modulo_id = models.CharField(primary_key=True, max_length=40)
     activo = models.BooleanField(default=True)
     class Meta:
         managed = False
         db_table = 'task_modulos'
+        unique_together = ('sistema', 'subsistema', 'modulo_id')
         verbose_name = u'Modulo'
         verbose_name_plural = u'Modulos'
         ordering = ('sistema', 'subsistema', 'modulo_id',)
     def __unicode__(self):
-        return self.modulo_id
+        return u"%s - %s - %s" % (self.sistema, self.subsistema, self.modulo_id)
 
 class Recurso(models.Model):
     recurso_id = models.CharField(primary_key=True, max_length=40)
@@ -106,7 +132,7 @@ class Recurso(models.Model):
         verbose_name_plural = u'Recursos'
         ordering = ('recurso_id',)
     def __unicode__(self):
-        return self.recurso_id
+        return u"%s - %s" % (self.recurso_id, self.nombre_completo)
 
 class RecursoSistema(models.Model):
     recurso = models.ForeignKey(Recurso)
@@ -115,6 +141,12 @@ class RecursoSistema(models.Model):
     class Meta:
         managed = False
         db_table = 'task_recursos_sistemas'
+        unique_together = ('recurso', 'sistema')
+        verbose_name = u'Recurso Sistema'
+        verbose_name_plural = u'Recursos Sistema'
+        ordering = ('recurso', 'sistema',)
+    def __unicode__(self):
+        return u"%s - %s" % (self.sistema, self.recurso)
 
 class ColorIssue(models.Model):
     color_id = models.CharField(primary_key=True, max_length=20)
@@ -123,10 +155,15 @@ class ColorIssue(models.Model):
     class Meta:
         managed = False
         db_table = 'task_colores_issue'
+        verbose_name = u'Color Issue'
+        verbose_name_plural = u'Colores Issue'
+        ordering = ('color_id',)
+    def __unicode__(self):
+        return u"%s - %s" % (self.color_id, self.hexadecimal)
 
 class Issue(models.Model):
     issue_id = models.IntegerField(primary_key=True)
-    recurso = models.ForeignKey(Recurso)
+    recurso = models.ForeignKey(Recurso, related_name='Issue_recursos')
     segmento = models.CharField(max_length=40)
     empresa = models.ForeignKey(Agencia, related_name='Issue_empresas')
     agencia = models.ForeignKey(Agencia, related_name='Issue_agencias')
@@ -145,16 +182,21 @@ class Issue(models.Model):
     estado_issue = models.CharField(max_length=40)
     pct_avance = models.IntegerField()
     fase_issue = models.CharField(max_length=40)
-    color = models.ForeignKey(ColorIssue)
+    color = models.ForeignKey(ColorIssue, related_name='Issue_coloresissue')
     observaciones = models.CharField(max_length=2000, blank=True)
     activo = models.BooleanField(default=True)
     class Meta:
         managed = False
         db_table = 'task_issues'
+        verbose_name = u'Issue'
+        verbose_name_plural = u'Issues'
+        ordering = ('issue_id',)
+    def __unicode__(self):
+        return u"%s - %s - %s - %s - %s - %s - %s" % (self.issue_id, self.recurso, self.segmento, self.empresa, self.sistema, self.subsistema, self.tipo_issue)
 
 class IssueReferencia(models.Model):
-    referencia_id = models.IntegerField(primary_key=True)
     issue = models.ForeignKey(Issue)
+    referencia_id = models.IntegerField(primary_key=True)
     solicitante = models.CharField(max_length=60)
     patrocinador = models.CharField(max_length=60)
     fecha_solicitud = models.DateTimeField()
@@ -166,10 +208,16 @@ class IssueReferencia(models.Model):
     class Meta:
         managed = False
         db_table = 'task_issues_referencias'
+        unique_together = ('issue', 'referencia_id')
+        verbose_name = u'Issue Referencia'
+        verbose_name_plural = u'Issue Referencias'
+        ordering = ('issue', 'referencia_id',)
+    def __unicode__(self):
+        return u"%s - %s - %s" % (self.issue, self.referencia_id, self.solicitante)
 
 class IssueActividad(models.Model):
-    actividad_id = models.IntegerField(primary_key=True)
     issue = models.ForeignKey(Issue)
+    actividad_id = models.IntegerField(primary_key=True)
     recurso = models.ForeignKey(Recurso)
     descripcion = models.CharField(max_length=4000)
     fecha_actividad = models.DateTimeField()
@@ -177,11 +225,12 @@ class IssueActividad(models.Model):
     class Meta:
         managed = False
         db_table = 'task_issues_actividades'
+        unique_together = ('issue', 'actividad_id')
         verbose_name=u'Issue Actividad'
         verbose_name_plural = u'Issues Actividades'
-        ordering = ('actividad_id',)
+        ordering = ('issue', 'actividad_id',)
     def __unicode__(self):
-        return self.actividad_id
+        return u"%s - %s - %s" % (self.issue, self.actividad_id, self.recurso)
 
 class IssueEmpresa(models.Model):
     issue = models.ForeignKey(Issue)
@@ -192,4 +241,9 @@ class IssueEmpresa(models.Model):
     class Meta:
         managed = False
         db_table = 'task_issues_empresas'
-
+        unique_together = ('issue', 'empresa')
+        verbose_name = u'Issue Empresa'
+        verbose_name_plural = u'Issue Empresas'
+        ordering = ('issue', 'empresa',)
+    def __unicode__(self):
+        return u"%s - %s" % (self.issue, self.Empresa)
